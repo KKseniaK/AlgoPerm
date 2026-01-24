16a86fc4ca058a8bd53a2375bff371809e3edb5a
Ksenia Krotova <ksenia.krotova2020@gmail.com>
2026-01-25 03:04:13 +0500

    start commit

--- seminar-01/obj/Debug/net8.0/seminar-01.AssemblyInfo.cs
+++ seminar-01/obj/Debug/net8.0/seminar-01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seminar-01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a02b3ec065bc4fa5a5080d5562575b643f3da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab39858e2526499a89bfdcb42ffec6c82955eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("seminar-01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seminar-01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ seminar-22/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace seminar_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
